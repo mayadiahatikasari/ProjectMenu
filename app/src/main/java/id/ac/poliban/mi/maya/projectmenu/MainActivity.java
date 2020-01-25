f1f7f512075f5b5ac435169e668f8cb8d55ceacb
mayadiah atikasari <mayadiahatikasari@gmail.com>
2020-01-25 19:29:37 +0800

    Initial commit

---
+++ app/src/main/java/id/ac/poliban/mi/maya/projectmenu/MainActivity.java
@@ -0,0 +1,41 @@
+package id.ac.poliban.mi.maya.projectmenu;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater =
+                getMenuInflater();
+        inflater.inflate(R.menu.optionmenu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.menu1) {
+            startActivity(new Intent(this, Menu1.class));
+        } else if (item.getItemId() == R.id.menu2) {
+            startActivity(new Intent(this, Menu2.class));
+        } else if (item.getItemId() == R.id.menu3) {
+            startActivity(new Intent(this, Menu3.class));
+        }
+        return true;
+    }
+}
+
+
